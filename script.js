4bf4fd7f372297b1f41582e62a622e1853535e93
Aleksandre Gvianidze <aleksandregvianidze@gmail.com>
2026-09-19 12:47:41 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+console.log("JavaScript is connected!");
+
+// TASK 1
+
+const productName = "Mechanical Keyboard";
+
+let productPrice = 250;
+
+let productQuantity = 4;
+
+let isAvailable = true;
+
+const brand = "Keychron";
+
+console.log(productName)
+console.log(productPrice)
+console.log(productQuantity)
+console.log(isAvailable)
+console.log(brand)
+
+
+// TASK 2
+
+const country = "Georgia";
+
+const city = "Tbilisi";
+
+let tripDays = 10;
+
+let budget = 2000;
+
+let hasHotel = true;
+
+console.log(country)
+console.log(city)
+console.log(tripDays)
+console.log(budget)
+console.log(hasHotel)
+
+
+
+// TASK 3
+
+const birthYear = 2004;
+
+let gameScore = 100;
+
+const firstName = "Aleksandre";
+
+let unreadMessages = "";
+
+let isOnline = "";
+
+console.log(birthYear)
+console.log(gameScore)
+console.log(firstName)
+console.log(unreadMessages)
+console.log(isOnline)
+
+
+// Challange 1
+
+
+const message = "I really like Python!";
+
+console.log(message)
+
+const newMessage = message.replace("Python","Javascript");
+
+console.log(newMessage)
+
+
+
+// Challange 2
+
+const producttPrice = 149.56789;
+
+console.log(producttPrice)
+
+const fixedPrice = producttPrice.toFixed(2)
+
+console.log(fixedPrice)
+
+
+
+
+
+
+
+
+
+
